e58efd79ec2b13754f2fa520daf3a25b7584160e
Vasyl Yevdoshchek <Vasyl.Yevdoshchek@gmail.com>
2022-12-03 21:15:19 +0200

    test

--- main.py
+++ main.py
@@ -6,8 +6,3 @@ os.chdir(r"C:\Users\User\Downloads")
 tree = ET.parse('test_new.xml')
 x = ET.Element("Items").findall("./TDM_Item_AbutmentKit/M-BASE direct")
 print(x)
-# root = tree.getroot()
-# print(root.tag)
-# print(root.attrib)
-# for child in root:
-#     print(child.tag, child.attrib)
