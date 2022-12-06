f42ebdceec162b69c68c1ca2372edb48f427a901
Vasyl Yevdoshchek <Vasyl.Yevdoshchek@3shape.com>
2022-12-06 10:35:47 +0200

    new version

--- main.py
+++ main.py
@@ -1,36 +1,47 @@
 import os
 import xml.etree.ElementTree as ET
+import sys
 
-os.chdir(r"C:\Users\User\Downloads\xml_test")
+if len(sys.argv)>4:
+    filepath = r"C:\Users\User\Downloads\xml_test"
+    new_file = 'test_new.xml'
+    old_file = 'test_old.xml'
+else:
+    filepath = sys.argv[1]
+    new_file = sys.argv[2]
+    old_file = sys.argv[3]
 
-tree = ET.parse('test_new.xml')
+
+os.chdir(filepath)
+
+tree = ET.parse(new_file)
 root = tree.getroot()
 
 dict_new = {}
 
 for i in range(0, 2691):
     try:
-        dict_new[root[0][i][2].attrib["value"]]=root[0][i][49].attrib["value"]
+        dict_new[root[0][64][0][i][2].attrib["value"]]=root[0][64][0][i][49].attrib["value"]
     except IndexError:
         print(i)
         break
-
+print(dict_new)
 for i in dict_new.items():
     print(i)
 
 
-tree = ET.parse('test_old.xml')
+tree = ET.parse(old_file)
 root = tree.getroot()
 
 dict_old = {}
 
 for i in range(0, 2691):
     try:
-        dict_old[root[0][i][2].attrib["value"]]=root[0][i][49].attrib["value"]
+        dict_old[root[0][64][0][i][2].attrib["value"]]=root[0][64][0][i][49].attrib["value"]
     except IndexError:
         print(i)
         break
-
+print(dict_old)
 values_were_unchanged = []
 values_were_changed = {}
 systems_did_not_exist_in_the_old_file = []
@@ -48,16 +59,16 @@ for k, v in dict_new.items():
 with open("values_were_unchanged.csv", "w")  as file:
     for i in range(len(values_were_unchanged)):
         for k,v in values_were_unchanged[i].items():
-            file.write("{},  {}".format(k,v) + '\n')
+            file.write('"{}",  {}'.format(k,v) + '\n')
 
 with open("values_were_changed.csv", "w")  as file:
     for k,v in values_were_changed.items():
-        file.write("{},  {}".format(k,v) + '\n')
+        file.write('"{}",  {}'.format(k,v) + '\n')
 
 with open("systems_did_not_exist_in_the_old_file.csv", "w")  as file:
     for i in range(len(systems_did_not_exist_in_the_old_file)):
         for k,v in systems_did_not_exist_in_the_old_file[i].items():
-            file.write("{},  {}".format(k,v) + '\n')
+            file.write('"{}",  {}'.format(k,v) + '\n')
 
 # print(values_were_unchanged)
 # print('\n'*3)
